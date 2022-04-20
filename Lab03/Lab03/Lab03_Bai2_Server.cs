ec9278aa4b08c8b56b84d0f89d95e3ddfb299634
oldslowhand115 <phongkhuu115@gmail.com>
2022-04-20 19:56:59 +0700

    fix bai 2

--- Lab03/Lab03/Lab03_Bai2_Server.cs
+++ Lab03/Lab03/Lab03_Bai2_Server.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
-
+using System.Threading;
 
 namespace Lab03
 {
@@ -19,28 +19,29 @@ namespace Lab03
         {
             InitializeComponent();
         }
-        
-        private void btnListen_Click(object sender, EventArgs e)
+        public void StartUnsafeThread()
         {
-            /*
             int recv;
             byte[] data = new byte[1024];
             IPAddress serverIP = IPAddress.Parse("127.0.0.1");
             IPEndPoint ipep = new IPEndPoint(serverIP, 8080);
-            TcpListener listener = new TcpListener(ipep);
             Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             newsock.Bind(ipep);
             newsock.Listen(10);
-            Socket client = newsock.AcceptSocket();
-            IPEndPoint clienttep = (IPEndPoint)client.RemoteEndPoint;
+            Socket client = newsock.Accept();
             while (true)
             {
                 data = new byte[1024];
                 recv = client.Receive(data);
                 if (recv == 0) break;
                 string rcvMessage = Encoding.ASCII.GetString(data, 0, recv);
-                richTextBox1.Text = rcvMessage;
-            }*/
+                richTextBox1.Text += rcvMessage;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread serverThread = new Thread(new ThreadStart(StartUnsafeThread));
+            serverThread.Start();
         }
     }
 }
